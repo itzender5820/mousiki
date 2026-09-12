1791edbbff8f0474d7ba17e88a1fe298bd21d68b
Itx.ender <itz.ender5820@gmail.com>
2026-09-12 21:28:37 +0530

    Add locale setting for UTF-8 support
    Set locale to C.UTF-8 if not already set.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <clocale>
 #include "app.h"
 
 int main() {
+    if (!std::setlocale(LC_ALL, "")) {
+        std::setlocale(LC_ALL, "C.UTF-8");
+    } else {
+        const char* cur = std::setlocale(LC_CTYPE, nullptr);
+        if (cur && std::string(cur) == "C") {
+            std::setlocale(LC_ALL, "C.UTF-8");
+        }
+    }
+
     muisc::App app;
     return app.run();
 }
